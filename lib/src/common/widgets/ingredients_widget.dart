303f951a800029950c297efcf834b5de4a3aa4cd
Ram Delatina <r2am9d@gmail.com>
2022-10-05 17:12:39 +0800

    Added initial implementation of ingredients dialog

--- lib/src/common/widgets/ingredients_widget.dart
+++ lib/src/common/widgets/ingredients_widget.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter_animated_dialog/flutter_animated_dialog.dart';
 
@@ -54,15 +55,17 @@ class IngredientsWidget extends StatelessWidget {
                   onPressed: () async {
                     await showAnimatedDialog(
                       context: context,
-                      barrierDismissible: false,
                       builder: (BuildContext animDialogcontext) {
                         return _IngredientsListDialog(
                           animDialogContext: animDialogcontext,
+                          ingredients: ingredients,
                         );
                       },
+                      axis: Axis.vertical,
                       curve: Curves.easeInOut,
-                      animationType: DialogTransitionType.fadeScale,
-                      duration: const Duration(milliseconds: 500),
+                      animationType: DialogTransitionType.size,
+                      duration: const Duration(milliseconds: 350),
+                      barrierColor: AppColor().primaryDark20.withOpacity(.9),
                     );
                   },
                   style: ElevatedButton.styleFrom(
@@ -107,22 +110,86 @@ class IngredientsWidget extends StatelessWidget {
   }
 }
 
-class _IngredientsListDialog extends StatelessWidget {
-  const _IngredientsListDialog({required this.animDialogContext});
+class _IngredientsListDialog extends HookWidget {
+  const _IngredientsListDialog({
+    required this.animDialogContext,
+    required this.ingredients,
+  });
 
   final BuildContext animDialogContext;
+  final List<String> ingredients;
 
   @override
   Widget build(BuildContext context) {
-    return ClassicGeneralDialogWidget(
-      titleText: 'Title',
-      contentText: 'content',
-      onPositiveClick: () {
-        Navigator.of(animDialogContext).pop();
-      },
-      onNegativeClick: () {
-        Navigator.of(animDialogContext).pop();
-      },
+    final mQ = MediaQuery.of(animDialogContext);
+    final navigator = Navigator.of(animDialogContext);
+
+    final selectedIndexes = <int>[];
+
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxHeight: mQ.size.height * 0.75,
+      ),
+      child: ClassicListDialogWidget<_IngredientsListItemModel>(
+        showScrollbar: true,
+        titleText: 'Ingredients list',
+        listType: ListType.multiSelect,
+        selectedIndexes: selectedIndexes,
+        activeColor: AppColor().secondary,
+        dataList: List.generate(
+          ingredients.length,
+          (index) => _IngredientsListItemModel(
+            name: ingredients[index],
+            value: index,
+          ),
+        ),
+        actions: [
+          Container(
+            width: mQ.size.width,
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(16.0),
+                    backgroundColor: AppColor().primary,
+                  ),
+                  onPressed: () {
+                    // navigator.pop();
+                  },
+                  icon: const FaIcon(FontAwesomeIcons.solidListCheck),
+                  label: const Text('Select All'),
+                ),
+                ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(16.0),
+                    backgroundColor: AppColor().secondary,
+                  ),
+                  onPressed: () {
+                    navigator.pop();
+                  },
+                  icon: const FaIcon(FontAwesomeIcons.solidCheck),
+                  label: const Text('Confirm'),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
+}
+
+class _IngredientsListItemModel {
+  _IngredientsListItemModel({
+    required this.name,
+    required this.value,
+  });
+
+  String name;
+  int value;
+
+  @override
+  String toString() => name;
 }
