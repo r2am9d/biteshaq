86ddd181abc134ebcfde3cf94c1404ce7be305bc
Ram Delatina <r2am9d@gmail.com>
2022-10-20 00:15:09 +0800

    Added cupertino localization delegate

--- lib/app.dart
+++ lib/app.dart
@@ -58,6 +58,7 @@ class App extends StatelessWidget {
       localizationsDelegates: const [
         FormBuilderLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
       ],
       supportedLocales: FormBuilderLocalizations.delegate.supportedLocales,
